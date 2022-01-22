eddee855cfa906c455e8e80c2ded3b501c05ebde
2tle <iam@2tle.io>
2022-01-22 14:53:57 +0900

    update: add image & write func..

--- app/src/main/java/io/twotle/sunrinapple2/MainActivity.kt
+++ app/src/main/java/io/twotle/sunrinapple2/MainActivity.kt
@@ -6,11 +6,35 @@ import io.twotle.sunrinapple2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    var eggTouch = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+    }
+
+    fun touchEgg() {
+        if(eggTouch >0) eggTouch--
+        when(eggTouch) {
+            80 -> {
+                binding.ivEgg.setImageResource(R.drawable.egg2)
+            }
+            50 -> {
+                binding.ivEgg.setImageResource(R.drawable.egg3);
+            }
+            10 -> {
+                binding.ivEgg.setImageResource(R.drawable.egg4);
+            }
+            0 -> {
+                binding.ivEgg.setImageResource(R.drawable.fry);
+            }
+        }
+    }
+
+    fun restart() {
+        eggTouch = 100
+        binding.ivEgg.setImageResource(R.drawable.egg);
     }
 
 
