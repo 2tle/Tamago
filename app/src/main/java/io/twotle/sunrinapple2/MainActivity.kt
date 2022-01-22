6b0d0f59c097b5a969cbccfa72ef69808a2b73de
2tle <iam@2tle.io>
2022-01-22 14:48:29 +0900

    update: main xml

--- app/src/main/java/io/twotle/sunrinapple2/MainActivity.kt
+++ app/src/main/java/io/twotle/sunrinapple2/MainActivity.kt
@@ -2,10 +2,16 @@ package io.twotle.sunrinapple2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.twotle.sunrinapple2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
